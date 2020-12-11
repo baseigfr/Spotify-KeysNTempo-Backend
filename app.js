6eb75436c973586c46e2d9c28a9ba7f18f83fa7b
baseigfr <baseigfr@mtu.edu>
2020-12-11 08:14:36 -0600

    edited code to show it uses the Authorization example

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-/**
- * This is an example of a basic node.js script that performs
- * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts.
- *
- * For more information, read
- * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
- */
+// this application uses the Authorization example on Spotify API's documentation. 
 
 const express = require('express'); // Express web server framework
 const request = require('request'); // "Request" library
